431da19fd38251955b7c6e7b1476ce67b6becfac
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-04-29 21:31:11 +0100

    STOP FORGETTING TO COMMIT YOU FORGETFUL PIECE OF SHIT

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -1,14 +1,16 @@
-const learnMoreBtn = document.querySelector('.smooth_scroll');
+const smoothScrollLinks = document.querySelectorAll('.smooth_scroll');
 
-learnMoreBtn.addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent default jump
+smoothScrollLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default jump
 
-  const targetSection = document.getElementById(this.getAttribute('href').slice(1)); // Get target element
+    const targetSection = document.getElementById(this.getAttribute('href').slice(1)); // Get target element
 
-  const scrollOptions = {
-    behavior: "smooth",
-    block: "start" // Optionally adjust scroll position (start, center, end)
-  };
+    const scrollOptions = {
+      behavior: "smooth",
+      block: "start" // Optionally adjust scroll position (start, center, end)
+    };
 
-  targetSection.scrollIntoView(scrollOptions); // Smooth scroll to target
+    targetSection.scrollIntoView(scrollOptions); // Smooth scroll to target
+  });
 });
